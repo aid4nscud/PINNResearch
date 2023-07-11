1ffb48da010fdf87d984a1a81f03209eb7dd20ff
Aidan Scudder <aidan.scudder@gmail.com>
2023-07-11 16:50:55 -0700

    syntax fix

--- DeepXDE/PINN.py
+++ DeepXDE/PINN.py
@@ -6,9 +6,9 @@ import tensorflow as tf
 
 
 # Check if TensorFlow is using GPU
-print("GPU DEVICES:\n\n"
+print("GPU DEVICES:\n\n")
 print(tf.config.list_physical_devices('GPU'))
-print("\n------------------------------------------\n"
+print("\n------------------------------------------\n")
 
 # Parameters
 alpha = 1.0
