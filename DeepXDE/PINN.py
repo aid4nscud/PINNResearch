f91f178d28e8bd1b8ef0973329151627983c15d3
Aidan Scudder <aidan.scudder@gmail.com>
2023-07-11 11:43:09 -0700

    added borah gpu run script

--- DeepXDE/PINN.py
+++ DeepXDE/PINN.py
@@ -13,8 +13,6 @@ geom = dde.geometry.Interval(0, length)
 timedomain = dde.geometry.TimeDomain(0,max_time)
 geotime = dde.geometry.GeometryXTime(geom, timedomain)
 
-
-
 # PDE Residual
 def pde(x, y):
     dy_t = dde.grad.jacobian(y , x, j=1)
@@ -22,14 +20,19 @@ def pde(x, y):
     return dy_t - dy_xx * alpha
 
 # Initial Condidtion & Boundary Condition
-def func(x):
-    return np.sin(np.pi * x[:,0:1] * np.exp(-x[:,1:]))
+def func_bc(x):
+    # Apply condition for left and right boundaries
+    return np.where(np.isclose(x[:, 0], length), 100.0, 0.0)[:, None]
 
-bc = dde.DirichletBC(geotime, func, lambda _, on_boundary:on_boundary)
-ic = dde.IC(geotime, func, lambda _, on_initial:on_initial)
+def func_ic(x):
+    # The initial condition is zero everywhere
+    return np.zeros((len(x), 1))
+
+bc = dde.DirichletBC(geotime, func_bc, lambda _, on_boundary: on_boundary)
+ic = dde.IC(geotime, func_ic, lambda _, on_initial: on_initial)
 
 # Training Data
-data = dde.data.TimePDE(geotime, pde, [bc, ic], num_domain=4000, num_boundary=2000, num_initial=1000, solution=func, num_test=1000)
+data = dde.data.TimePDE(geotime, pde, [bc, ic], num_domain=4000, num_boundary=2000, num_initial=1000, num_test=1000)
 
 # Model Architecture
 layer_size = [2] + [32]*3 + [1]
@@ -38,17 +41,16 @@ initializer = "Glorot uniform"
 optimizer = "adam"
 learning_rate = 0.001
 
-#Compile Model
+#Compile and Train Model
 net = dde.nn.FNN(layer_size, activation, initializer)
 model = dde.Model(data, net)
-model.compile(optimizer, learning_rate, metrics=["l2 relative error"] )
-
-# Train Model
-losshistory, train_state = model.train(iterations=10000)
+model.compile(optimizer, learning_rate)
+model.train(10000)
+model.compile("L-BFGS")
+losshistory, train_state = model.train()
 
 # Results
-
-x_data = np.linspace(-1, 1, num = 100)
+x_data = np.linspace(0, length, num = 100)
 t_data = np.linspace(0, 1, num=100)
 test_x, test_t = np.meshgrid(x_data,t_data)
 test_domain = np.vstack((np.ravel(test_x), np.ravel(test_t))).T
---
+++ DeepXDE/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J python           # job name
+#SBATCH -o log_slurm.o%j    # output and error file name (%j expands to jobID)
+#SBATCH -n 1                # total number of tasks requested
+#SBATCH -N 1                # number of nodes you want to run on
+#SBATCH -p bsudfq           # queue (partition) for R2 use defq
+#SBATCH -t 1:00:00         # run time (hh:mm:ss) - 12.0 hours in this example.
+
+# Activate the conda environment
+# Replace environmentName with your environment name
+
+. ~/.bashrc
+
+conda activate pinn
+
+python PINN.py
