3c44f87777987ad58c468047f66e69b11b03f5a3
Aidan Scudder <aidan.scudder@gmail.com>
2023-07-13 20:07:43 -0700

    changes

--- DeepXDE/PINN.py
+++ DeepXDE/PINN.py
@@ -17,15 +17,14 @@ timedomain = dde.geometry.TimeDomain(0, max_time)
 geotime = dde.geometry.GeometryXTime(geom, timedomain)
 
 # PDE Residual
-def pde(x, y, t, u, u_t):
-    u_xx = dde.grad.hessian(u, x, y, t)[0, 0]
-    u_yy = dde.grad.hessian(u, x, y, t)[1, 1]
-    return u_t - alpha * (u_xx + u_yy)
+def pde(x, u, ux, uy, ut):
+    u_xx = dde.grad.hessian(u, x)[0, 0]
+    u_yy = dde.grad.hessian(u, x)[1, 1]
+    return ut - alpha * (u_xx + u_yy)
 
 # Initial Condition & Boundary Condition
-def func_bc_right_edge(x):
-    # Assign a value of 100.0 if the point lies on the right edge and 0.0 otherwise
-    return np.where(np.isclose(x[:, 0], length), 100.0, 0.0)[:, None]
+def func_bc_right_edge(x, on_boundary):
+    return on_boundary and np.isclose(x[0], length)
 
 def func_ic(x):
     # The initial condition is zero everywhere
@@ -35,17 +34,21 @@ def func_zero(x):
     return np.zeros_like(x)
 
 bc_right_edge = dde.DirichletBC(
-    geotime, func_bc_right_edge, lambda _, on_boundary: on_boundary
+    geotime, lambda x: 100, func_bc_right_edge
 )
+
 bc_left = dde.NeumannBC(
-    geotime, func_zero, lambda x, on_boundary: on_boundary and np.isclose(x[0], 0) and not np.isclose(x[1], width) and not np.isclose(x[1], 0)
+    geotime, func_zero, lambda x, on_boundary: on_boundary and np.isclose(x[0], 0)
 )
+
 bc_top = dde.NeumannBC(
-    geotime, func_zero, lambda x, on_boundary: on_boundary and np.isclose(x[1], width) and not np.isclose(x[0], length) and not np.isclose(x[0], 0)
+    geotime, func_zero, lambda x, on_boundary: on_boundary and np.isclose(x[1], width)
 )
+
 bc_bottom = dde.NeumannBC(
-    geotime, func_zero, lambda x, on_boundary: on_boundary and np.isclose(x[1], 0) and not np.isclose(x[0], length) and not np.isclose(x[0], 0)
+    geotime, func_zero, lambda x, on_boundary: on_boundary and np.isclose(x[1], 0)
 )
+
 ic = dde.IC(geotime, func_ic, lambda _, on_initial: on_initial)
 
 # Training Data
@@ -58,26 +61,16 @@ data = dde.data.TimePDE(
     num_initial=320,
     num_test=5060,
 )
-pde_resampler = dde.callbacks.PDEPointResampler(period=50)
 
-# Define the custom PINN model
-class CustomPINN(dde.maps.FNN):
-    def forward(self, x):
-        u = self.net(x)
-        x, y, t = dde.split_dim(x, 3)
-        u_x = dde.grad.jacobian(u, x)
-        u_y = dde.grad.jacobian(u, y)
-        u_t = dde.grad.jacobian(u, t)
-        return u, u_x, u_y, u_t
+pde_resampler = dde.callbacks.PDEPointResampler(period=50)
 
 # Optimizer and Learning Rate
 optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)  # Using Adam optimizer
 
-
 # Compile and Train Model
-net = CustomPINN([2, 50, 50, 50, 50, 50, 1], "tanh", "Glorot uniform")
+net = dde.maps.FNN([3] + [50] * 5 + [1], "tanh", "Glorot uniform")
 model = dde.Model(data, net)
-model.compile(optimizer, 1e-4)
+model.compile(optimizer, loss_weights=[1e-4], check_numerics=True)
 model.train(iterations=100000, callbacks=[pde_resampler])
 
 # Results
