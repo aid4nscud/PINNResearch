319dfec31a658f37bb69506a32eded123782c31f
Aidan Scudder <aidan.scudder@gmail.com>
2023-07-18 01:51:48 -0700

    change

--- DeepXDE/PINN.py
+++ DeepXDE/PINN.py
@@ -9,7 +9,7 @@ ALPHA = 1.0
 LENGTH = 1.0
 WIDTH = 1.0
 MAX_TIME = 1.0
-LAYER_SIZE = [3] + [64] * 8 + [1]
+LAYER_SIZE = [3] + [64] * 16 + [1]
 ACTIVATION = "tanh"
 INITIALIZER = "Glorot uniform"
 OPTIMIZER = "L-BFGS"
@@ -56,7 +56,7 @@ def main():
     # Define Neural Network Architecture and Model
     net = dde.nn.FNN(LAYER_SIZE, ACTIVATION, INITIALIZER)
     model = dde.Model(data, net)
-    model.compile("L-BFGS-B", {"maxiter": 1000, "gtol": 1e-8, "ftol": 1e-7})
+    model.compile("L-BFGS-B", {"maxiter": 1000, "gtol": 1e-6, "ftol": 1e-6})
 
 
     # Train Model
