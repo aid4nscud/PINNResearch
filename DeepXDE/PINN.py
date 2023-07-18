a7892bd0b579fa4d1d5c680d4dad509111fe6776
Aidan Scudder <aidan.scudder@gmail.com>
2023-07-18 01:16:41 -0700

    change

--- DeepXDE/PINN.py
+++ DeepXDE/PINN.py
@@ -49,7 +49,7 @@ def main():
 
     # Define Training Data
     data = dde.data.TimePDE(geotime, pde, [bc_right_edge, bc_left, bc_top, bc_bottom, ic],
-                             num_domain=8000, num_boundary=2000, num_initial=3000, num_test=1000)
+                             num_domain=16000, num_boundary=4000, num_initial=6000, num_test=2000)
     pde_resampler = dde.callbacks.PDEPointResampler(period=50)
 
     # Define Neural Network Architecture and Model
