2d59930600922713463b1e969e3fa6218ce4f92b
Aidan Scudder <aidan.scudder@gmail.com>
2023-07-17 17:19:59 -0700

    change

--- DeepXDE/test.py
+++ DeepXDE/test.py
@@ -37,11 +37,11 @@ def main():
         return du_t - diff_coef * (du_xx + du_yy)
 
     # Define Boundary Conditions
-    def bc_hot(_, on_boundary):
-        return on_boundary and np.isclose(_, plate_length)
+    def bc_hot(X, on_boundary):
+        return np.logical_and(on_boundary, np.isclose(X[:, 0], plate_length))
 
-    def bc_cold(_, on_boundary):
-        return on_boundary and not (np.isclose(_, 0) or np.isclose(_, plate_length))
+    def bc_cold(X, on_boundary):
+        return np.logical_and(on_boundary, ~np.logical_or(np.isclose(X[:, 0], 0), np.isclose(X[:, 0], plate_length)))
 
     bc_right_edge = dde.DirichletBC(geotime, lambda X: 100.0, bc_hot)
     bc_left = dde.NeumannBC(geotime, lambda X: 0.0, bc_cold)
