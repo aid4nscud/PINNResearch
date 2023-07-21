e640102c7c2a5b5b945c01248a3a4d9b214bd2c3
Aidan Scudder <aidan.scudder@gmail.com>
2023-07-20 22:17:31 -0700

    ok

--- DeepXDE/Heat2d.py
+++ DeepXDE/Heat2d.py
@@ -67,7 +67,7 @@ lr = 1e-3
 # Applying Loss weights as given below
 # [PDE Loss, BC1 loss - Dirichlet Left , BC2 loss - Dirichlet Right, BC3 loss- Neumann up, BC4 loss - Neumann down, IC Loss]
 loss_weights = [10, 1, 1, 1, 1, 10]
-epochs = 100000
+epochs = 10000
 optimizer = "adam"
 batch_size_ = 256
 
