d7624b31792dcaec5d7f80ab54074510a205b4b7
Aidan Scudder <aidan.scudder@gmail.com>
2023-07-25 08:32:14 -0700

    revert

--- DeepXDE/Heat2d.py
+++ DeepXDE/Heat2d.py
@@ -6,66 +6,56 @@ import matplotlib.animation as animation
 from matplotlib.animation import FuncAnimation
 
 # Some useful functions
-t1 = 0
-t2 = 1
-end_time = 1
+START = 0
+END_TIME = 1
+ALPHA = 1.0
 
 
 def pde(X, T):
     dT_xx = dde.grad.hessian(T, X, j=0)
     dT_yy = dde.grad.hessian(T, X, j=1)
     dT_t = dde.grad.jacobian(T, X, j=2)
-    #     Dividing by rhoc to make it 1
-    ALPHA = 1.0
-    # no forcing function
+
     return dT_t - (ALPHA * (dT_xx + dT_yy))
 
 
 def r_boundary(X, on_boundary):
-    x, y, t = X
+    x, _, _ = X
     return on_boundary and np.isclose(x, 1)
 
 
 def l_boundary(X, on_boundary):
-    x, y, t = X
+    x, _, _ = X
     return on_boundary and np.isclose(x, 0)
 
 
 def up_boundary(X, on_boundary):
-    x, y, t = X
+    _, y, _ = X
     return on_boundary and np.isclose(y, 1)
 
 
 def down_boundary(X, on_boundary):
-    x, y, t = X
+    _, y, _ = X
     return on_boundary and np.isclose(y, 0)
 
 
 def boundary_initial(X, on_initial):
-    x, y, t = X
+    _, _, t = X
     return on_initial and np.isclose(t, 0)
 
 
 def init_func(X):
-    return np.zeros((len(X), 1))
-
-
-def dir_func_l(X):
-    return t1 * np.ones((len(X), 1))
+    t = np.zeros((len(X), 1))
+    return t
 
 
 def dir_func_r(X):
-    return t2 * np.ones((len(X), 1))
+    # IMPORTANT: The boundary condition is scaled down to be between 0 and 1 (1 for this scenario) so that the NN can minimize all of the diff losses effectively using Tanh.
+    return np.ones((len(X), 1))
 
 
 def func_zero(X):
     return np.zeros((len(X), 1))
-
-
-def hard(X, T):
-    x, y, t = x[:, 0:1], x[:, 1:2], x[:, 2:3]
-
-    return (r - r_in) * y + T_star
 
 
 num_domain = 30000
@@ -75,24 +65,31 @@ layer_size = [3] + [60] * 5 + [1]
 activation_func = "tanh"
 initializer = "Glorot uniform"
 lr = 1e-3
-# Applying Loss weights as given below
-# [PDE Loss, BC1 loss - Dirichlet Left , BC2 loss - Dirichlet Right, BC3 loss- Neumann up, BC4 loss - Neumann down, IC Loss]
-loss_weights = [10, 1, 1, 1, 1, 10]
-epochs = 10000
+loss_weights = [
+    10,
+    1,
+    1,
+    1,
+    1,
+    10,
+]  # [PDE Loss, BC1 loss - Neumann Left , BC2 loss - Dirichlet Right, BC3 loss- Neumann up, BC4 loss - Neumann down, IC Loss]
+iterations = 10000
 optimizer = "adam"
 batch_size_ = 256
 
+# Space + Time Domain
 geom = dde.geometry.Rectangle(xmin=[0, 0], xmax=[1, 1])
-timedomain = dde.geometry.TimeDomain(0, end_time)
+timedomain = dde.geometry.TimeDomain(0, END_TIME)
 geomtime = dde.geometry.GeometryXTime(geom, timedomain)
 
+# Boundary + Initial Conditions
 bc_l = dde.NeumannBC(geomtime, func_zero, l_boundary)
 bc_r = dde.DirichletBC(geomtime, dir_func_r, r_boundary)
 bc_up = dde.NeumannBC(geomtime, func_zero, up_boundary)
 bc_low = dde.NeumannBC(geomtime, func_zero, down_boundary)
 ic = dde.IC(geomtime, init_func, boundary_initial)
 
-
+# Data Source
 data = dde.data.TimePDE(
     geomtime,
     pde,
@@ -102,28 +99,28 @@ data = dde.data.TimePDE(
     num_initial=num_initial,
 )
 
-
+# Define Model
 net = dde.maps.FNN(layer_size, activation_func, initializer)
-net.apply_output_transform(lambda x, y: abs(y))
-## Uncomment below line to apply hard Dirichlet Boundary Conditions
-net.apply_output_transform(lambda x, y: x[:,0:1]*t2 + (1 - x[:,0:1]) * y)
+net.apply_output_transform(lambda _, y: abs(y))
 model = dde.Model(data, net)
 
 model.compile(optimizer, lr=lr, loss_weights=loss_weights)
-# To save the best model every 1000 epochs
-checker = dde.callbacks.ModelCheckpoint(
-    "model/model1.ckpt", save_better_only=True, period=1000
-)
+
+# Train Model
 losshistory, trainstate = model.train(
-    epochs=epochs, batch_size=batch_size_, callbacks=[checker]
+    iterations=iterations,
+    batch_size=batch_size_,
 )
+# Re-Compile with L-BFGS Optimizer
 model.compile("L-BFGS-B")
 dde.optimizers.set_LBFGS_options(
     maxcor=50,
 )
-losshistory, train_state = model.train(epochs=epochs, batch_size=batch_size_)
+# Train again
+losshistory, train_state = model.train(iterations=iterations, batch_size=batch_size_)
 dde.saveplot(losshistory, trainstate, issave=True, isplot=True)
 
+# Test and build animation from each predicted time frame
 fig, ax = plt.subplots()
 ax = fig.add_subplot(111)
 nelx = 100
@@ -158,24 +155,22 @@ for time in t:
     T = T.reshape(nelx + 1, nely + 1)
     Ts.append(T)
 
-
 def plotheatmap(T, time):
     # Clear the current plot figure
     plt.clf()
-    plt.title(f"Temperature at t = {time*delta_t} unit time")
+    plt.title(f"Temperature at t = {round(time*delta_t, ndigits=2)}")
     plt.xlabel("x")
     plt.ylabel("y")
     plt.pcolor(xx, yy, T, cmap="jet")
     plt.colorbar()
     return plt
 
-
 def animate(k):
     plotheatmap(Ts[k], k)
 
-
 anim = animation.FuncAnimation(
-    plt.figure(), animate, interval=1, frames=len(t), repeat=False
+    plt.figure(), animate, interval=100, frames=len(t), repeat=False
 )
 
-anim.save("pinn_solution.gif")
+anim.save("pinn_heat2d_solution.mp4", writer="ffmpeg")
+
