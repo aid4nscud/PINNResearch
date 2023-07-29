a9f4e358cf5b0c7a4d463067f640132b0aae511f
Aidan Scudder <aidan.scudder@gmail.com>
2023-07-29 15:53:58 -0700

    normalizing initial condition

--- DeepXDE/AllenCahn.py
+++ DeepXDE/AllenCahn.py
@@ -82,7 +82,7 @@ def init_func(X):
     t = np.random.uniform(
         -0.05, 0.05, (len(X), 1)
     )  # Temperature is randomly distributed between -0.05 and 0.05 everywhere at the start
-    return t
+    return t * 20
 
 
 # Define Neumann boundary condition
@@ -163,6 +163,9 @@ for time in t:
     X = np.column_stack((x_, y_))  # Making 2d array with x and y
     X = np.column_stack((X, t_))  # Making 3d array with the 2d array and t
     T = model.predict(X)  # Predict the solution
+    T = (
+        T / 20
+    )  # Rescale the predicted temperature back to the original scale of initial condition
     T = T.reshape(
         T.shape[0],
     )
