3f0373b10f3ea6be8050ef174bd2bb0aa48122d5
Aidan Scudder <aidan.scudder@gmail.com>
2023-08-04 09:47:18 -0700

    ok

--- DeepXDE/run.sh
+++ DeepXDE/run.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=python         # Job name
 #SBATCH --output=log_slurm.%j.out # Output file
 #SBATCH --error=log_slurm.%j.err  # Error file
-#SBATCH --nodes=1                 # Number of nodes
+#SBATCH --nodes=2                # Number of nodes
 #SBATCH --ntasks-per-node=1       # Number of tasks per node
-#SBATCH --gres=gpu:1              # Number of GPUs
+#SBATCH --gres=gpu:2              # Number of GPUs
 #SBATCH --partition=gpu           # Partition/queue name
 #SBATCH --time=1:00:00            # time limit
 
