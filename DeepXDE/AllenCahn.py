dc1f147432f2ce74c920c52514916fa879a92342
Aidan Scudder <aidan.scudder@gmail.com>
2023-08-06 02:57:43 -0700

    norm

--- DeepXDE/AllenCahn.py
+++ DeepXDE/AllenCahn.py
@@ -48,10 +48,16 @@ def boundary_initial(X, on_initial):
 def init_func(X):
     x, y, _ = X[:, 0:1], X[:, 1:2], X[:, 2]
     sine_pattern = np.sin(np.pi * x) * np.sin(np.pi * y)
-    noise = np.random.uniform(
-        -0.05, 0.05, (len(X), 1)
-    )  # Reduce the amplitude of the noise
-    return sine_pattern + noise
+
+    # Normalizing the pattern to the range [-1, 1]
+    normalized_pattern = (
+        2
+        * (sine_pattern - sine_pattern.min())
+        / (sine_pattern.max() - sine_pattern.min())
+        - 1
+    )
+
+    return normalized_pattern
 
 
 # Define Neumann boundary condition
