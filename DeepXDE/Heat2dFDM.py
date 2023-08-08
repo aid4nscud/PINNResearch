1cc8925f80172eaccb6ccbae8a1bc7f3eb4c9fa6
Aidan Scudder <aidan.scudder@gmail.com>
2023-08-08 14:58:31 -0700

    perfected FDM for any diffusivity (ALPHA)

--- DeepXDE/Heat2dFDM.py
+++ DeepXDE/Heat2dFDM.py
@@ -6,14 +6,14 @@ W = 1
 H = 1
 Nx = 101
 Ny = 101
-ALPHA = 0.01
-total_time = 1
-dt = 0.001
-output_times = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+ALPHA = 1  # You can now change ALPHA dynamically
 
 # Discretization
 dx = W / (Nx - 1)
 dy = H / (Ny - 1)
+dt = min(dx**2, dy**2) / (4 * ALPHA)  # Dynamically computed time step
+total_time = 1
+output_times = np.linspace(0, total_time, 11)  # You can modify the number of output times as needed
 
 # Mesh grid
 x = np.linspace(0, W, Nx)
@@ -28,7 +28,7 @@ time = 0
 for output_time in output_times:
     while time < output_time:
         T_new = T.copy()
-        
+
         # Internal nodes
         for i in range(1, Nx - 1):
             for j in range(1, Ny - 1):
@@ -49,7 +49,7 @@ for output_time in output_times:
         time += dt
 
     # Visualization
-    plt.contourf(X, Y, T.T, cmap="jet", levels=100, vmin=0, vmax=100)
+    plt.contourf(X, Y, T.T, cmap="jet", levels=100, vmin=T.min(), vmax=T.max())
     plt.colorbar()
     plt.title(f"Temperature Distribution at t={output_time}")
     plt.xlabel("x-direction")
