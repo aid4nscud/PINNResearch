6d4c37144907082308a7d14435a9d6c8355e6b42
Aidan Scudder <aidan.scudder@gmail.com>
2023-08-08 16:05:04 -0700

    data generation program

--- DeepXDE/Heat2dFDM.py
+++ DeepXDE/Heat2dFDM.py
@@ -1,57 +1,99 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.io
 
-# Domain parameters
-W = 1
-H = 1
-Nx = 101
-Ny = 101
-ALPHA = 1  # You can now change ALPHA dynamically
+data_list = []
+num_runs = 5000
 
-# Discretization
-dx = W / (Nx - 1)
-dy = H / (Ny - 1)
-dt = min(dx**2, dy**2) / (4 * ALPHA)  # Dynamically computed time step
-total_time = 1
-output_times = np.linspace(0, total_time, 11)  # You can modify the number of output times as needed
+for run in range(1, num_runs):
+    print(str.format("Starting run {}", run))
+    # Domain parameters
+    W = 1
+    H = 1
+    Nx = 101
+    Ny = 101
+    ALPHA = 100 / num_runs  # You can now change ALPHA dynamically
 
-# Mesh grid
-x = np.linspace(0, W, Nx)
-y = np.linspace(0, H, Ny)
-X, Y = np.meshgrid(x, y)
+    # Discretization
+    dx = W / (Nx - 1)
+    dy = H / (Ny - 1)
+    dt = min(dx**2, dy**2) / (4 * ALPHA)  # Dynamically computed time step
+    total_time = 1
+    output_times = np.linspace(
+        0, total_time, 11
+    )  # You can modify the number of output times as needed
 
-# Initial condition
-T = np.zeros((Nx, Ny))
+    # Mesh grid
+    x = np.linspace(0, W, Nx)
+    y = np.linspace(0, H, Ny)
+    X, Y = np.meshgrid(x, y)
 
-# Time-stepping loop
-time = 0
-for output_time in output_times:
-    while time < output_time:
-        T_new = T.copy()
+    # Initial condition
+    T = np.zeros((Nx, Ny))
 
-        # Internal nodes
-        for i in range(1, Nx - 1):
-            for j in range(1, Ny - 1):
-                T_new[i, j] = T[i, j] + ALPHA * dt * (
-                    (T[i + 1, j] - 2 * T[i, j] + T[i - 1, j]) / dx**2
-                    + (T[i, j + 1] - 2 * T[i, j] + T[i, j - 1]) / dy**2
-                )
+    # Time-stepping loop
 
-        # Neumann boundary conditions (zero-flux)
-        T_new[0, :] = T_new[1, :]
-        T_new[:, 0] = T_new[:, 1]
-        T_new[:, -1] = T_new[:, -2]
+    x_data = []
+    y_data = []
+    t_data = []
+    T_data = []
 
-        # Dirichlet boundary condition
-        T_new[-1, :] = 100
+    time = 0
+    for output_time in output_times:
+        while time < output_time:
+            T_new = T.copy()
 
-        T = T_new
-        time += dt
+            # Internal nodes
+            for i in range(1, Nx - 1):
+                for j in range(1, Ny - 1):
+                    T_new[i, j] = T[i, j] + ALPHA * dt * (
+                        (T[i + 1, j] - 2 * T[i, j] + T[i - 1, j]) / dx**2
+                        + (T[i, j + 1] - 2 * T[i, j] + T[i, j - 1]) / dy**2
+                    )
 
-    # Visualization
-    plt.contourf(X, Y, T.T, cmap="jet", levels=100, vmin=T.min(), vmax=T.max())
-    plt.colorbar()
-    plt.title(f"Temperature Distribution at t={output_time}")
-    plt.xlabel("x-direction")
-    plt.ylabel("y-direction")
-    plt.show()
+            # Neumann boundary conditions (zero-flux)
+            T_new[0, :] = T_new[1, :]
+            T_new[:, 0] = T_new[:, 1]
+            T_new[:, -1] = T_new[:, -2]
+
+            # Dirichlet boundary condition
+            T_new[-1, :] = 100
+
+            T = T_new
+            time += dt
+
+            # Append the data for the current output time
+            for i in range(Nx):
+                for j in range(Ny):
+                    x_data.append(x[i])
+                    y_data.append(y[j])
+                    t_data.append(output_time)
+                    T_data.append(T[i, j])
+
+        # Visualization
+        # plt.contourf(X, Y, T, cmap="jet", levels=100, vmin=T.min(), vmax=T.max())
+        # plt.colorbar()
+        # plt.title(f"Temperature Distribution at t={output_time}")
+        # plt.xlabel("x-direction")
+        # plt.ylabel("y-direction")
+        # plt.show()
+
+    # Append the data
+    for i in range(Nx):
+        for j in range(Ny):
+            x_data.append(x[i])
+            y_data.append(y[j])
+            t_data.append(output_time)
+            T_data.append(T[i, j])
+
+    # Convert lists to arrays
+    data_dict = {
+        "x_data": np.array(x_data),
+        "y_data": np.array(y_data),
+        "t_data": np.array(t_data),
+        "T_data": np.array(T_data),
+    }
+    data_list.append(data_dict)
+
+    # Save to .mat file
+scipy.io.savemat("temperature_data.mat", {"data": data_list})
---
+++ DeepXDE/InverseHeat2d.py
@@ -0,0 +1,141 @@
+import deepxde as dde
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
+import re
+
+# true values
+T_START = 0
+T_END = WIDTH = LENGTH = ALPHA = 1.0
+
+
+# Load training data
+def load_training_data(num):
+    data = loadmat("data_file.mat")  # Replace with the appropriate mat file path
+
+    x = data["x"][:num].reshape(-1, 1)
+    y = data["y"][:num].reshape(-1, 1)
+    t = data["t"][:num].reshape(-1, 1)
+    T = data["T"][:num].reshape(-1, 1)  # Temperature data
+
+    return x, y, t, T
+
+
+# Parameters to be identified
+ALPHA = dde.Variable(0.0)
+
+
+# Define PDE
+def pde(X, T):
+    # Calculate second derivatives (Hessians) of T with respect to X in both dimensions
+    dT_xx = dde.grad.hessian(T, X, i=0, j=0)
+    dT_yy = dde.grad.hessian(T, X, i=1, j=1)
+
+    # Calculate first derivative (Jacobian) of T with respect to X in time dimension
+    dT_t = dde.grad.jacobian(T, X, j=2)
+
+    # Return the defined PDE
+    return dT_t - (ALPHA * dT_xx + dT_yy)
+
+
+# Define boundary conditions
+def boundary_right(X, on_boundary):
+    x, _, _ = X
+    return on_boundary and np.isclose(x, WIDTH)  # Check if on the right boundary
+
+
+def boundary_left(X, on_boundary):
+    x, _, _ = X
+    return on_boundary and np.isclose(x, 0)  # Check if on the left boundary
+
+
+def boundary_top(X, on_boundary):
+    _, y, _ = X
+    return on_boundary and np.isclose(y, LENGTH)  # Check if on the upper boundary
+
+
+def boundary_bottom(X, on_boundary):
+    _, y, _ = X
+    return on_boundary and np.isclose(y, 0)  # Check if on the lower boundary
+
+
+# Define initial condition
+def boundary_initial(X, on_initial):
+    _, _, t = X
+    return on_initial and np.isclose(t, 0)  # Check if at the initial time
+
+
+# Initialize a function for the temperature field
+def init_func(X):
+    t = np.zeros((len(X), 1))  # Temperature is zero everywhere at the T_START
+    return t
+
+
+# Define Dirichlet and Neumann boundary conditions
+def constraint_right(X):
+    return np.ones((len(X), 1))  # On the right boundary, temperature is kept at 1
+
+
+def func_zero(X):
+    return np.zeros(
+        (len(X), 1)
+    )  # On the other boundaries, the derivative of temperature is kept at 0 (Neumann condition)
+
+
+# Define geometry and time domains
+geom = dde.geometry.Rectangle([0, 0], [WIDTH, LENGTH])  # Geometry domain
+timedomain = dde.geometry.TimeDomain(0, T_END)  # Time domain
+geomtime = dde.geometry.GeometryXTime(geom, timedomain)  # Space-time domain
+
+# Define boundary conditions and initial condition
+bc_l = dde.NeumannBC(geomtime, func_zero, boundary_left)  # Left boundary
+bc_r = dde.DirichletBC(geomtime, constraint_right, boundary_right)  # Right boundary
+bc_up = dde.NeumannBC(geomtime, func_zero, boundary_top)  # Upper boundary
+bc_low = dde.NeumannBC(geomtime, func_zero, boundary_bottom)  # Lower boundary
+ic = dde.IC(geomtime, init_func, boundary_initial)  # Initial condition
+
+# Get the training data: num = 5000
+ob_x, ob_y, ob_t, ob_T = load_training_data(num=5000)
+ob_xyt = np.hstack((ob_x, ob_y, ob_t))
+observe_T = dde.icbc.PointSetBC(ob_xyt, ob_T, component=0)
+# Training datasets and Loss
+data = dde.data.TimePDE(
+    geomtime,
+    pde,
+    [bc_l, bc_r, bc_up, bc_low, observe_T],
+    num_domain=700,
+    num_boundary=200,
+    num_initial=100,
+    anchors=ob_xyt,
+)
+
+# Neural Network setup
+layer_size = [3] + [50] * 6 + [3]
+activation = "tanh"
+initializer = "Glorot uniform"
+net = dde.nn.FNN(layer_size, activation, initializer)
+model = dde.Model(data, net)
+
+# callbacks for storing results
+fnamevar = "variables.dat"
+variable = dde.callbacks.VariableValue([ALPHA], period=100, filename=fnamevar)
+
+# Compile, train and save model
+model.compile("adam", lr=1e-3, external_trainable_variables=ALPHA)
+loss_history, train_state = model.train(
+    iterations=10000,
+    callbacks=[variable],
+    display_every=1000,
+    disregard_previous_best=True,
+)
+
+model.compile("adam", lr=1e-4, external_trainable_variables=[ALPHA])
+loss_history, train_state = model.train(
+    iterations=10000,
+    callbacks=[variable],
+    display_every=1000,
+    disregard_previous_best=True,
+)
+dde.saveplot(loss_history, train_state, issave=True, isplot=True)
+plt.show()
+plt.savefig("loss_history_plot_InverseHeat2d")
