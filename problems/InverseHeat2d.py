37fbc45ff406a6b3924cfb2a2c22062941713507
Aidan Scudder <aidan.scudder@gmail.com>
2023-08-27 18:27:28 -0600

    save plot

--- problems/InverseHeat2d.py
+++ problems/InverseHeat2d.py
@@ -1,8 +1,11 @@
 import deepxde as dde
 import numpy as np
 from deepxde.backend import tf
+import matplotlib.pyplot as plt  # For creating static, animated, and interactive visualizations in Python
+
 
 ALPHA = dde.Variable(1.0)  # Initialize the value of alpha
+
 
 def pde(x, y):
     dy_t = dde.grad.jacobian(y, x, i=0, j=2)
@@ -10,14 +13,17 @@ def pde(x, y):
     dy_yy = dde.grad.hessian(y, x, i=0, j=1)
     return dy_t - ALPHA * (dy_xx + dy_yy)
 
+
 # Load the data
 data_dict = np.load("temperature_data.npz")
-X_data = data_dict['x_data']
-Y_data = data_dict['y_data']
-T_data = data_dict['T_data']
+X_data = data_dict["x_data"]
+Y_data = data_dict["y_data"]
+T_data = data_dict["T_data"]
 
 # Flatten and stack to create observation points
-observe_x = np.stack(np.meshgrid(X_data.flatten(), Y_data.flatten(), 1), -1).reshape(-1, 3)
+observe_x = np.stack(np.meshgrid(X_data.flatten(), Y_data.flatten(), 1), -1).reshape(
+    -1, 3
+)
 observe_y = T_data.flatten()[:, None]
 
 # Define the spatial and temporal domain
@@ -32,14 +38,14 @@ data = dde.data.TimePDE(
     geomtime,
     pde,
     [observe_y_bc],
-    num_domain=40,
-    num_boundary=20,
-    num_initial=10,
+    num_domain=1000,
+    num_boundary=500,
+    num_initial=250,
     anchors=observe_x,
     num_test=10000,
 )
 
-layer_size = [3] + [32] * 3 + [1]
+layer_size = [3] + [60] * 5 + [1]
 activation = "tanh"
 initializer = "Glorot uniform"
 net = dde.nn.FNN(layer_size, activation, initializer)
@@ -53,3 +59,6 @@ variable = dde.callbacks.VariableValue(ALPHA, period=1000)
 losshistory, train_state = model.train(iterations=50000, callbacks=[variable])
 
 dde.saveplot(losshistory, train_state, issave=True, isplot=True)
+plt.show()
+plt.savefig("loss_history_inverseHeat2d")
+plt.close()
