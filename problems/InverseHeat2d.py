c68a55d779082d6b57bd4415806d92006f317628
Aidan Scudder <aidan.scudder@gmail.com>
2023-08-27 20:40:51 -0600

    wrong spelling of parameter name

--- problems/InverseHeat2d.py
+++ problems/InverseHeat2d.py
@@ -157,7 +157,7 @@ losshistory, train_state = model.train(
     iterations=ITERATIONS, batch_size=BATCH_SIZE, callbacks=[variable]
 )
 
-model.compile("L-BFGS-B", external_training_variables=[ALPHA])
+model.compile("L-BFGS-B", external_trainable_variables=[ALPHA])
 dde.optimizers.set_LBFGS_options(
     maxcor=100,
 )
